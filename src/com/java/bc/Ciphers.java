185e63b7bdc9acaa9b576358d4f47b68dac99e60
usernamehui <myzhangpenghui@163.com>
2019-05-24 19:22:58 +0800

    aaaa

--- src/com/java/bc/Ciphers.java
+++ src/com/java/bc/Ciphers.java
@@ -1,18 +1,48 @@
 package com.java.bc;
 
+import java.security.SecureRandom;
+
 import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 public class Ciphers {
+	
+	public static SecretKey privateKey;//私钥key
+	public static String type = "AES";//加密类型
+	
+	/**
+	 * 初始化私钥Key
+	 */
+	static {
+		KeyGenerator instance;
+		try {
+			int count = 0;
+			switch(type) {
+				case "AES" : count = 128;break;
+				case "DES" : count = 56;break;
+			}
+			instance = KeyGenerator.getInstance(type);
+			instance.init(count,new SecureRandom("".getBytes()));
+			privateKey = instance.generateKey();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 
 	public static void main(String[] args) {
 		
+		/**
+		 * AES 加密 = 16位
+		 * DES 加密= 56位
+		 */
+		
 		//jdk AES 加密
-		System.out.println(new String(jdkCipher("AES", "ABC".getBytes(), true)));
+		System.out.println(new String(jdkCipher("abcdefg".getBytes(), true)));
 		
 		//jdk AES 解密
-		System.out.println(new String(jdkCipher("AES", jdkCipher("AES", "ABC".getBytes(), true), false)));
-		
+		System.out.println(new String(jdkCipher(jdkCipher("abcdefg".getBytes(), true), false)));
 		
 		
 		
@@ -44,16 +74,13 @@ public class Ciphers {
 	 * @param isDecode	是否加密(true加密  false解密)
 	 * @return
 	 */
-	public static byte[] jdkCipher(String type,byte[] str,boolean isDecode) {
-		/**
-		 * java自带加密方式 
-		 */
+	public static byte[] jdkCipher(byte[] str,boolean isDecode) {
 		try {
 			if(isDecode) {//加密
 				//构建 AES 加密算法对象
 				Cipher cipher = Cipher.getInstance(type);
-				//初始化为加密类型 -- 16位字符串的填充长度<加解密需一致>
-				cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec("0000000000000000".getBytes("UTF-8"), type));
+				//初始化操作模式 加密类型 -- AES密钥为 16位字符串<加解密需一致>
+				cipher.init(Cipher.ENCRYPT_MODE, privateKey);
 				//加密
 				byte[] enDoFinal = cipher.doFinal(str);
 //				System.out.println("AES 加密："+new String(enDoFinal));
@@ -63,8 +90,8 @@ public class Ciphers {
 			if(!isDecode) {//解密
 				//构建 AES 加密算法对象
 				Cipher cipher = Cipher.getInstance(type);
-				//初始化为解密类型 -- 16位字符串的填充长度<加解密需一致>
-				cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec("0000000000000000".getBytes("UTF-8"), type));
+				//初始化操作模式 解密类型 -- AES密钥为 16位字符串<加解密需一致>
+				cipher.init(Cipher.DECRYPT_MODE, privateKey);
 				//解密
 				byte[] deDoFinal = cipher.doFinal(str);
 //				System.out.println("AES 解密："+new String(deDoFinal));
@@ -76,5 +103,7 @@ public class Ciphers {
 		}
 		return null;
 	}
+	
+
 
 }
